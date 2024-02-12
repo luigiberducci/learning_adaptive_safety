correct computation forward raceline, implement vec polylines, relative obs, add r/l distances to centerline

gym_envs/multi_agent_env/common/track.py
@@ -43,6 +43,8 @@ class Raceline:
         xs: np.ndarray,
         ys: np.ndarray,
         velxs: np.ndarray,
+        drights: np.ndarray = None,
+        dlefts: np.ndarray = None,
         ss: np.ndarray = None,
         psis: np.ndarray = None,
         kappas: np.ndarray = None,
@@ -59,6 +61,9 @@ class Raceline:
         self.kappas = kappas
         self.velxs = velxs
         self.accxs = accxs
+        self.drights = drights
+        self.dlefts = dlefts
+
 
         # approximate track length by linear-interpolation of x,y waypoints
         # note: we could use 'ss' but sometimes it is normalized to [0,1], so we recompute it here
@@ -81,17 +86,23 @@ class Raceline:
             10 if "General1" in filepath.name else 1
         )  # General1 has very bad centerline, need to be smoothed
         xx, yy = waypoints[::step, 0], waypoints[::step, 1]
+
         # close loop
         xx = np.append(xx, xx[0])
         yy = np.append(yy, yy[0])
         spline = CubicSpline2D(xx, yy)
         ds = 0.1
 
-        ss, xs, ys, yaws, ks = [], [], [], [], []
+        ss, xs, ys, yaws, ks, ddr, ddl = [], [], [], [], [], [], []
         smoothyaws, smoothks = [], []
 
         for i_s in np.arange(0, spline.s[-1], ds):
             x, y = spline.calc_position(i_s)
+            pos = np.array([x, y])
+            _, _, _, i = nearest_point(pos, waypoints[:, :2])
+            i = i % waypoints.shape[0]
+            dr = waypoints[i, 2]
+            dl = waypoints[i, 3]
 
             yaw = spline.calc_yaw(i_s)
             k = spline.calc_curvature(i_s)
@@ -101,6 +112,8 @@ class Raceline:
 
             xs.append(x)
             ys.append(y)
+            ddr.append(dr)
+            ddl.append(dl)
             yaws.append(yaw)
             ks.append(k)
             ss.append(i_s)
@@ -112,6 +125,8 @@ class Raceline:
             xs=np.array(xs),
             ys=np.array(ys),
             velxs=np.ones_like(ss) * fixed_speed,
+            drights=np.array(ddr),
+            dlefts=np.array(ddl),
             psis=np.array(smoothyaws),
             kappas=np.array(smoothks),
             spline=spline,
@@ -308,18 +323,47 @@ def extract_forward_raceline(
     dist = 0.0
     final_waypoint_id = begin_i
     while dist < forward_distance:
-        final_waypoint_id = (final_waypoint_id + 1) % trajectory_xyv.shape[0]
+        final_waypoint_id = final_waypoint_id + 1
+        final_waypoint_id_norm = final_waypoint_id % trajectory_xyv.shape[0]
         dist += np.linalg.norm(
-            trajectory_xyv[final_waypoint_id, :2]
-            - trajectory_xyv[final_waypoint_id - 1, :2]
+            trajectory_xyv[final_waypoint_id_norm, :2]
+            - trajectory_xyv[final_waypoint_id_norm - 1, :2]
         )
 
     forward_xyvs = np.empty((n_points, 3))
-    for i in range(n_points):
-        idx = (begin_i + i) % trajectory_xyv.shape[0]
+    delta_i = int((final_waypoint_id - begin_i) // n_points)
+    for i in np.arange(n_points):
+        idx = (begin_i + i * delta_i) % trajectory_xyv.shape[0]
         forward_xyvs[i] = trajectory_xyv[idx]
 
     return forward_xyvs
+
+@njit(cache=True)
+def extract_forward_boundary(
+    trajectory_xy: np.ndarray,
+    first_point: np.ndarray,
+    last_point: np.ndarray,
+    n_points: int = 10,
+) -> np.ndarray:
+    """extract forward curvature starting a given point on the track"""
+    _, _, _, begin_i = nearest_point(first_point, trajectory_xy)
+    _, _, _, end_i = nearest_point(last_point, trajectory_xy)
+
+    # compute distance between first and last point
+    dist = 0.0
+    for i in range(begin_i, end_i):
+        dist += np.linalg.norm(
+            trajectory_xy[i, :2]
+            - trajectory_xy[i - 1, :2]
+        )
+
+    # sample n points equally spaced
+    forward_xys = np.empty((n_points, 2))
+    for i in range(n_points):
+        idx = (begin_i + i * (end_i - begin_i) // n_points) % trajectory_xy.shape[0]
+        forward_xys[i] = trajectory_xy[idx, :2]
+
+    return forward_xys
 
 
 if __name__ == "__main__":

gym_envs/wrappers/observation_wrappers.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 from typing import Dict, Any
 
 import gymnasium as gym
+import matplotlib.pyplot as plt
 import numpy as np
 
 from gym_envs.multi_agent_env.common.track import (
     extract_forward_curvature,
-    extract_forward_raceline,
+    extract_forward_raceline, extract_forward_boundary, Raceline,
 )
 from gym_envs.multi_agent_env import MultiAgentRaceEnv
 from gym_envs.multi_agent_env.common.utils import traj_global2local
@@ -52,14 +53,9 @@ class VehicleTrackObservationWrapper(gym.ObservationWrapper):
         super().__init__(env)
 
         # observation configuration
-        self.vehicle_features = (
-            vehicle_features
-            if vehicle_features is not None
-            else ["pose", "velocity", "frenet_coords"]
-        )
-        self.track_features = (
-            track_features if track_features is not None else ["curvature"]
-        )
+        self.vehicle_features = vehicle_features or ["pose", "velocity", "frenet_coords"]
+        self.track_features = track_features or ["raceline"]
+
         self.forward_curv_lookahead = forward_curv_lookahead  # meters
         self.n_curvature_points = (
             n_curvature_points  # number of curvature points to sample
@@ -109,6 +105,18 @@ class VehicleTrackObservationWrapper(gym.ObservationWrapper):
             ],
             axis=1,
         )
+
+        xys = np.stack([centerline.xs, centerline.ys], axis=1)
+        xys = np.concatenate([xys, [xys[0]]], axis=0)
+        dxys = xys[1:] - xys[:-1]
+        normals = np.stack([-dxys[:, 1], dxys[:, 0]], axis=1)
+        normals /= np.linalg.norm(normals, axis=1)[:, None]
+
+        dlefts = centerline.dlefts
+        drights = centerline.drights
+
+        self.left_boundary_xy = xys[:-1] + dlefts[:, None] * normals
+        self.right_boundary_xy = xys[:-1] - drights[:, None] * normals
 
     def track_feature_space_factory(self, feature_name: str) -> gym.spaces.Box:
         if feature_name == "curvature":
@@ -184,50 +192,116 @@ class VehicleTrackObservationWrapper(gym.ObservationWrapper):
         return obs
 
 
-class VectorTrackObservationWrapper(gym.ObservationWrapper):
-    def __init__(
-        self,
-        env: gym.Env,
-        vehicle_features: list[str] = None,
-        track_features: list[str] = None,
-        forward_lookahead: float = 10.0,
-        n_points: int = 10,
-        n_observed_vehicles: int = 1,
-    ):
-        super().__init__(env)
-
-        self.vehicle_features = vehicle_features or ["pose", "velocity"]
-        self.track_features = track_features or ["left_boundary", "right_boundary"]
-
-        self.forward_lookahead = forward_lookahead
-        self.n_points = n_points
-        self.n_observed_vehicles = n_observed_vehicles
-
-        # check observation features are present in environment
-        for agent_id in self.agents_ids:
-            for feature in self.vehicle_features:
-                assert (
-                    feature in env.observation_space[agent_id].spaces
-                ), f"{feature} not in obs-space of {agent_id}"
-
-        # prepare new observation space
-        # vehicle features
-        obs_dict = {}
-        # todo relative observation
-
-        # track features
-        for feature in self.track_features:
-            obs_dict[feature] = self.track_feature_space_factory(feature)
-
-        self.observation_space = gym.spaces.Dict(obs_dict)
+class VectorTrackObservationWrapper(VehicleTrackObservationWrapper):
 
     def track_feature_space_factory(self, feature_name: str) -> gym.spaces.Box:
         if feature_name == "left_boundary":
-            return gym.spaces.Box(low=-10, high=10, shape=(self.n_points, 2))
-        if feature_name == "right_boundary":
-            return gym.spaces.Box(low=-10, high=10, shape=(self.n_points, 2))
+            return gym.spaces.Box(
+                low=-np.inf, high=np.inf, shape=(self.n_raceline_points, 2), dtype=np.float32
+            )
+        elif feature_name == "right_boundary":
+            return gym.spaces.Box(
+                low=-np.inf, high=np.inf, shape=(self.n_raceline_points, 2), dtype=np.float32
+            )
+        elif feature_name == "vec_raceline":
+            return gym.spaces.Box(
+                low=-np.inf, high=np.inf, shape=(self.n_raceline_points, 3), dtype=np.float32
+            )
         else:
-            raise ValueError(f"unknown track feature {feature_name}")
+            return super().track_feature_space_factory(feature_name)
+
+    def observation(self, observation):
+        obs = {}
+        ego_pose = observation["ego"]["pose"]
+        all_poses = [observation[agent]["pose"] for agent in self.agents_ids]
+        distances = np.linalg.norm(np.array(all_poses)[:, :2] - ego_pose[:2], axis=1)
+        closest_ids = np.argsort(distances)[: self.n_vehicles]
+        for i, nid in enumerate(closest_ids):
+            orig_agent_id = self.agents_ids[nid]
+            new_agent_id = "ego" if i == 0 else f"vehicle_nearby{i}"
+            obs[new_agent_id] = {}
+            for feature in self.vehicle_features:
+                # feature relative to ego
+                ego_feature = np.array(observation["ego"][feature])
+                other_feature = np.array(observation[orig_agent_id][feature])
+                if feature == "pose":
+                    # transform pose to ego frame
+                    rel_pos = traj_global2local(ego_pose=ego_feature, traj=other_feature[:2][None])
+                    rel_head = other_feature[2] - ego_feature[2]
+                    rel_feature = np.array([rel_pos[0][0], rel_pos[0][1], rel_head])
+                else:
+                    rel_feature = other_feature - ego_feature
+                obs[new_agent_id][feature] = rel_feature
+        # zero padding for missing vehicles
+        for i in range(self.n_vehicles - len(closest_ids)):
+            new_agent_id = f"vehicle_nearby{len(closest_ids) + i}"
+            obs[new_agent_id] = {}
+            for feature in self.vehicle_features:
+                obs[new_agent_id][feature] = np.zeros_like(
+                    observation["ego"][feature]
+                )
+        for feature in self.track_features:
+            obs[feature] = self.observe_context_feature(feature, observation)
+
+        return obs
+
+    def observe_context_feature(
+            self, feature_name: str, obs: Dict[str, Any]
+    ) -> np.ndarray:
+        if feature_name == "vec_raceline":
+            """
+            extract forward raceline from the track centerline.
+            the raceline profile is sampled at a fixed interval (5 wps) for a fixed length (10 samples).
+            so in total, the raceline profile is 5*10 waypoints long (approx. 10 meters in General1).
+            """
+            ego_pose = obs["ego"]["pose"]
+            ego_vel = obs["ego"]["velocity"]
+            ego_xyv = np.concatenate([ego_pose[:2], ego_vel])
+
+            waypoints = extract_forward_raceline(
+                trajectory_xyv=self.raceline_xyv,
+                point=ego_xyv[:2],
+                forward_distance=self.forward_curv_lookahead,
+                n_points=self.n_raceline_points,
+            )
+
+            # vectorize the waypoints first -> vector between ego pose and first waypoint,
+            # then vector between first and second waypoint, and so on.
+            rel_waypoints = traj_global2local(ego_pose, waypoints[:, :2])
+            rel_waypoints[1:] -= rel_waypoints[:-1]
+            rel_waypoints = np.concatenate([rel_waypoints, waypoints[:, 2:]], axis=1)
+            rel_waypoints[:, 2] -= ego_vel  # relative velocity
+
+            return rel_waypoints
+        elif feature_name in ["left_boundary", "right_boundary"]:
+            ego_pose = obs["ego"]["pose"]
+
+            raceline_wp = extract_forward_raceline(
+                trajectory_xyv=self.raceline_xyv,
+                point=ego_pose[:2],
+                forward_distance=self.forward_curv_lookahead,
+                n_points=self.n_raceline_points,
+            )
+
+            first, last = raceline_wp[0, :2], raceline_wp[-1, :2]
+
+            all_boundary = self.left_boundary_xy if feature_name == "left_boundary" else self.right_boundary_xy
+            boundary = extract_forward_boundary(
+                trajectory_xy=all_boundary,
+                first_point=first,
+                last_point=last,
+                n_points=self.n_raceline_points,
+            )
+
+            # vectorize the boundary
+            rel_boundary = traj_global2local(ego_pose, boundary)
+            rel_boundary[1:] -= rel_boundary[:-1]
+
+            return rel_boundary
+        else:
+            return super().observe_context_feature(feature_name, obs)
+
+
 
 
 if __name__ == "__main__":
@@ -243,8 +317,22 @@ if __name__ == "__main__":
         render_mode="human",
     )
 
-    env = VehicleTrackObservationWrapper(env)
-    #env = VectorTrackObservationWrapper(env)
+    # rendering
+    def render_waypoints(e):
+        """
+        Callback to render waypoints.
+        """
+        points = np.stack([track.raceline.xs, track.raceline.ys], axis=1)
+        e.render_closed_lines(points, color=(128, 0, 0), size=1)
+
+    env.add_render_callback(render_waypoints)
+
+    #env = VehicleTrackObservationWrapper(env)
+    env = VectorTrackObservationWrapper(
+        env,
+        vehicle_features=["pose", "frenet_coords", "velocity"],
+        track_features=["vec_raceline", "raceline", "left_boundary", "right_boundary"]
+    )
 
     print("action space:")
     print(env.action_space)
@@ -253,8 +341,52 @@ if __name__ == "__main__":
 
     obs, _ = env.reset(options={"mode": "random_back"})
     done = False
+    debug = False
 
+    t = 0
     while not done:
+        t += 1
         action = env.action_space.sample()
         obs, reward, done, _, info = env.step(action)
+
+        # debug
+        raceline = obs["raceline"]
+        vec_raceline = obs["vec_raceline"]
+        vec_lboundary = obs["left_boundary"]
+        vec_rboundary = obs["right_boundary"]
+
+        # plot arrows from 0,0 to rl points
+        if debug and t % 50 == 0:
+            fig, axes = plt.subplots(1, 2)
+            ax0, ax1 = axes
+
+            ax0.clear()
+            for i in range(raceline.shape[0]):
+                ax0.arrow(0, 0, raceline[i, 0], raceline[i, 1], head_width=0.02)
+
+            # plot arrows from 0,0 to first rl point, then from first to second, etc.
+            ax1.clear()
+            for vec_polyline in [vec_raceline, vec_lboundary, vec_rboundary]:
+                starting_pt = np.array([0.0, 0.0])
+                for i in range(vec_polyline.shape[0] - 1):
+                    starting_pt += vec_polyline[i][:2]
+                    ax1.arrow(starting_pt[0], starting_pt[1], vec_polyline[i + 1, 0], vec_polyline[i + 1, 1],
+                              head_width=0.02)
+
+            # plot opp
+            for agent_id, agent_obs in obs.items():
+                if agent_id in env.track_features:
+                    continue
+                ax1.scatter(agent_obs["pose"][0], agent_obs["pose"][1], label=agent_id)
+
+            ax0.set_aspect('equal')
+            ax1.set_aspect('equal')
+
+            # invert y axis
+            ax0.set_ylim(ax0.get_ylim()[::-1])
+            ax1.set_ylim(ax1.get_ylim()[::-1])
+
+            plt.legend()
+            plt.show()
+
         env.render()